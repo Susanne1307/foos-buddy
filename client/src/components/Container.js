734ea5016999f51fc1e62154802a7cb0d915b490
Susanne1307 <susanneholocher@mail.de>
2020-04-10 16:31:40 +0200

    Add container component

---
+++ client/src/components/Container.js
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const Container = styled.section`
+  background: none;
+  display: flex;
+  flex-flow: column nowrap;
+  margin: auto;
+  width: 80%;
+  max-width: 300px;
+`;
+
+export default Container;
