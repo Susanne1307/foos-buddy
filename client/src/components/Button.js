67690701ed16f546a4479b673d11f852cf4ec257
Susanne1307 <susanneholocher@mail.de>
2020-04-10 16:42:11 +0200

    Delete fix size data of the button to make it more generic

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,26 +1,24 @@
 import styled from '@emotion/styled';
 
-// Basic styling for all buttons ${(props) => getFontSize(props.size)};
 const Button = styled.button`
   text-align: center;
-  width: 280px;
-  height: 66px;
   background: #3f4859;
   color: #fafafa;
+  font-size: 26px;
+  padding: 15px 20px;
+  margin: 20px 0px;
   border: 3px #28afb0 solid;
   border-radius: 30px;
   box-shadow: 0px 15px 30px grey;
-  font-size: 26px;
-  padding: 10px 20px;
   outline: none;
-
   cursor: pointer;
   &:hover {
     color: #ffffff;
-    background: #6e7d9a;
+    background: #293141;
   }
   &:active {
-    background: #3f4859;
+    transform: translateY(1px);
+    filter: saturate(150%);
   }
 `;
 
