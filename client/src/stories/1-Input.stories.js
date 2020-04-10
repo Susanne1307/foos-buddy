946726c51da7130366274b11d9b80e3db036c280
Susanne1307 <susanneholocher@mail.de>
2020-04-10 16:34:39 +0200

    Delete needless code within stories

--- client/src/stories/1-Input.stories.js
+++ client/src/stories/1-Input.stories.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { LoginInput } from '../components/Input';
+import LoginInput from '../components/LoginInput';
+import ProfileInput from '../components/ProfileInput';
 
 export default {
-  title: 'LoginInput',
+  title: 'Input',
 };
 
 export const MailLogin = (props) => (
@@ -11,12 +12,4 @@ export const MailLogin = (props) => (
 export const PasswordLogin = (props) => (
   <LoginInput placeholder="********" type="password" {...props} />
 );
-export const MailSignUp = (props) => (
-  <LoginInput placeholder="Your mail" type="email" {...props} />
-);
-export const PasswordSignUp = (props) => (
-  <LoginInput placeholder="Your password" type="password" {...props} />
-);
-export const VerifyPassword = (props) => (
-  <LoginInput placeholder="Verify Password" type="password" {...props} />
-);
+export const NameInput = () => <ProfileInput placeholder="Name" />;
