6dc2974e2650df04af1f49ac79b6fe9ba27867f6
Susanne1307 <susanneholocher@mail.de>
2020-04-10 16:36:41 +0200

    Outsource LoginInput and ProfileInput to make input component more generic

---
+++ client/src/components/LoginInput.js
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+import Input from '../components/Input';
+
+const LoginInput = styled(Input)`
+  text-align: center;
+  background: white;
+  color: white;
+  font-size: 22px;
+  padding: 17px 0px;
+  margin: 20px 0px;
+  border: 3px #5ce0b9 solid;
+  border-radius: 30px;
+  box-shadow: 0px 15px 30px grey;
+  ::-webkit-input-placeholder {
+    text-align: center;
+    color: #b7b7b7;
+  }
+  &:focus {
+    background-color: #ebfff9;
+    color: #7c7c7c;
+  }
+`;
+
+export default LoginInput;
