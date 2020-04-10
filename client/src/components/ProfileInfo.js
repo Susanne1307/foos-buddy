6f3b3a99faab71bb9f56b3497ac29da880db61e9
Susanne1307 <susanneholocher@mail.de>
2020-04-10 16:39:23 +0200

    Create ProfilInfo styling with Storybook

---
+++ client/src/components/ProfileInfo.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const ProfileInfo = styled.p`
+  text-align: left;
+  background: none;
+  color: #fafafa;
+  border: none;
+  border-bottom: 3px #5ce0b9 solid;
+  font-size: 18px;
+  padding: 0px 0px 10px 10px;
+`;
+
+export default ProfileInfo;
---
+++ client/src/stories/3-ProfileInfo.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ProfileInfo from '../components/ProfileInfo';
+
+export default {
+  title: 'Profile',
+};
+
+export const ProfileName = () => <ProfileInfo>Max Mustermann</ProfileInfo>;
