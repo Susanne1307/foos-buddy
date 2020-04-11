fc32c799192b25676f5801fe8acb04fabe968440
Susanne1307 <susanneholocher@mail.de>
2020-04-11 12:59:40 +0200

    Replace React logo and favicon

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../src/assets/logo.svg';
 import './App.css';
 
 function App() {
@@ -7,17 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        ></a> */}
       </header>
     </div>
   );
