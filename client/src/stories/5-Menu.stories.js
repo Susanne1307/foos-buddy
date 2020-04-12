ebebe65b7e737b7205ae52f82836a8d9e42351cc
Susanne1307 <susanneholocher@mail.de>
2020-04-12 12:54:23 +0200

    Add side navigation

---
+++ client/src/stories/5-Menu.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SideNavigation from '../components/SideNavigation';
+
+export default {
+  title: 'Menu',
+};
+
+export const NavigationMenu = () => <SideNavigation />;
