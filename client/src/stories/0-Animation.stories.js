68cc1e7649bb1bef58a129165e041248edfbfa95
Susanne1307 <susanneholocher@mail.de>
2020-04-12 18:40:55 +0200

    Outsource start animation and add it to home screen

--- client/src/stories/0-Animation.stories.js
+++ client/src/stories/0-Animation.stories.js
@@ -1,23 +1,8 @@
 import React from 'react';
-import {
-  AnimationContainer,
-  StartAnimationRight,
-  StartAnimationLeft,
-  StartAnimationTitle,
-} from '../components/StartAnimation';
-import { Foos, Buddy } from '../components/Title';
+import StartAnimation from '../components/StartAnimation';
 
 export default {
   title: 'Animation',
 };
 
-export const animating = () => (
-  <AnimationContainer>
-    <StartAnimationLeft />
-    <StartAnimationRight />
-    <StartAnimationTitle>
-      <Foos>Foos</Foos>
-      <Buddy>Buddy</Buddy>
-    </StartAnimationTitle>
-  </AnimationContainer>
-);
+export const TestAnimation = () => <StartAnimation />;
