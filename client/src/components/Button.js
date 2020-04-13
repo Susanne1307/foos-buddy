cdc31099f928e01e229c6fcd4198471c8a4aaa3e
Susanne1307 <susanneholocher@mail.de>
2020-04-13 14:23:05 +0200

    Manage more colors in theme.js

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -9,11 +9,11 @@ const Button = styled.button`
   margin: 20px 0px;
   border: 3px ${(props) => props.theme.colors.primary} solid;
   border-radius: 30px;
-  box-shadow: 0px 15px 30px grey;
+  box-shadow: 0px 15px 30px ${(props) => props.theme.colors.shadow};
   outline: none;
   cursor: pointer;
   &:hover {
-    color: #ffffff;
+    color: ${(props) => props.theme.colors.textPrimaryHover};
     background: ${(props) => props.theme.colors.secondaryHover};
   }
   &:active {
--- client/src/theme.js
+++ client/src/theme.js
@@ -1,13 +1,18 @@
 const theme = {
   colors: {
-    textPrimary: '#FAFAFA',
-    textSecondary: '#B7B7B7',
     primary: '#28AFB0',
     secondary: '#3F4859',
     tertiary: '#5CE0B9',
     quaternary: '#D81E5B',
+    textPrimary: '#FAFAFA',
+    textSecondary: '#B7B7B7',
+    textPrimaryHover: '#ffffff',
     primaryHover: '#1f8e90',
     secondaryHover: '#293141',
+    shadow: 'grey',
+    loginFocus: '#ebfff9',
+    loginFocusText: '#7c7c7c',
+    profileInput: '#4d5665',
   },
 };
 
