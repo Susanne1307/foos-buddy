1eee19f6e01b7b9c619644a78af656e8900e97b7
Susanne1307 <susanneholocher@mail.de>
2020-04-14 17:58:52 +0200

    Connect to Mongo database

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,38 @@ import theme from './theme';
 import Start from '../src/pages/Start';
 import Login from '../src/pages/Login';
 
+const [test, setTest] = React.useState(null);
+
+React.useEffect(() => {
+  fetch('/api/test')
+    .then((response) => response.json())
+    .then((result) => {
+      setTest(result);
+    });
+}, []);
+
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Start />
-          </Route>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-        </Switch>
-      </Router>
-    </ThemeProvider>
+    <>
+      {test && (
+        <div>
+          {test.title}: {test.message}
+        </div>
+      )}
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Start />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </>
   );
 }
 
