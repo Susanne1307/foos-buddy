d4db65bb9ced0b783383013d323c56dfda899433
Susanne1307 <susanneholocher@mail.de>
2020-04-14 17:19:04 +0200

    Connect to mongo database

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config();
+process.env.MONGO_URL;
+
+const { MongoClient } = require('mongodb');
 const express = require('express');
 const path = require('path');
 
@@ -6,10 +11,30 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', (request, response) => {
-  response.sendFile(path.join(__dirname, 'client/build/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running http://localhost:${port}`);
+const client = new MongoClient(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+client.connect().then(async () => {
+  console.log('database connected 😍');
+  const db = client.db('FoosBuddy');
+  const users = await db.collection('users');
+  const oneUser = await users.findOne();
+  console.log('Found user', oneUser);
+  await users.insertOne({ firstName: 'Susanne' });
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+});
+
+app.get('/api/users', (request, response) => {
+  response.json({
+    title: 'Hello Tester',
+    message: 'Moin',
+  });
 });
