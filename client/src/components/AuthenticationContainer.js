2c2dccdee7b07a131839ad0d4b623a8b8acc76ff
Susanne1307 <susanneholocher@mail.de>
2020-04-15 17:16:28 +0200

    Outsource and rename container

---
+++ client/src/components/AuthenticationContainer.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const AuthenticationContainer = styled.section`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: space-evenly;
+  width: 80vw;
+  max-width: 300px;
+  height: 100vh;
+  max-height: 600px;
+`;
+
+export default AuthenticationContainer;
