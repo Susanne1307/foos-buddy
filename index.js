ac70502f2ae066295f846e2dd9dca183c6c08e98
Susanne1307 <susanneholocher@mail.de>
2020-04-20 18:11:21 +0200

    Create connection to mongoose

--- index.js
+++ index.js
@@ -1,34 +1,22 @@
 const dotenv = require('dotenv');
 dotenv.config();
 process.env.MONGO_URL;
-
-const { MongoClient } = require('mongodb');
-
 const express = require('express');
-const path = require('path');
-
 const port = process.env.PORT || 8080;
 const app = express();
+const mongoose = require('mongoose');
+const authRoute = require('./lib/routes/auth');
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log('Successfully connected with mongoose 🙂')
+);
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+app.use(express.json());
 
-const client = new MongoClient(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+app.use('/api/user', authRoute);
 
-client.connect().then(async () => {
-  console.log('database connected 😍');
-  const db = client.db('FoosBuddy');
-  const users = await db.collection('users');
-  const oneUser = await users.findOne();
-  console.log('Found user', oneUser);
-  await users.insertOne({ firstName: 'Susanne' });
-  app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port} 🎉`);
-  });
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port} 🎉`);
 });
