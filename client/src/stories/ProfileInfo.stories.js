3ede4cc9f42fc837674962c76fd7747bc9be9340
Susanne1307 <susanneholocher@mail.de>
2020-05-13 14:10:13 +0200

    Delete unused components

--- client/src/stories/ProfileInfo.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ProfileInfo from '../components/ProfileInfo';
-
-export default {
-  title: 'Profile',
-  component: 'Profile',
-};
-
-export const ProfileName = () => <ProfileInfo>Max Mustermann</ProfileInfo>;
