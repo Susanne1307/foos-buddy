b3b11c179b337ec8d7f7368cb95ecb3d8d0b2c10
susanneh <60884180+Susanne1307@users.noreply.github.com>
2020-05-28 12:43:44 +0200

    Fix react router for storybook (#56)

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,4 +1,6 @@
 import { configure, addDecorator } from '@storybook/react';
 import themeDecorator from './themeDecorator';
+import StoryRouter from 'storybook-react-router';
 
 addDecorator(themeDecorator);
+addDecorator(StoryRouter());
