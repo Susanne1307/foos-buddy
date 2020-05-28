2660c32bace81030b358e93c182eaca12c3bbce3
Susanne1307 <susanneholocher@mail.de>
2020-05-28 12:41:47 +0200

    Fix react router for storybook

--- client/src/stories/Menu.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import SideNavigation from '../components/SideNavigation';
-
-export default {
-  title: 'Navigation',
-  component: 'Navigation',
-};
-
-export const Navigation = () => <SideNavigation />;
